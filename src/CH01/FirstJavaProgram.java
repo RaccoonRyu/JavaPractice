5bc0c371cf4cf2a45fb93adda57625e9e3991c90
류지헌 <rjhwind94@gmail.com>
2022-04-28 09:17:46 +0900

    프로젝트 초안 작성

---
+++ src/CH01/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+package CH01;
+
+public class FirstJavaProgram {
+    // 첫 Java 프로그램 작성
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("First Java Program");
+    }
+}
