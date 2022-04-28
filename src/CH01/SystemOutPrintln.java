959b9d5e1128ea349fff189b8f6eb4455626b688
류지헌 <rjhwind94@gmail.com>
2022-04-28 10:30:08 +0900

    콘솔 화면 출력 예제인 SystemOutPrintln.java 작성

---
+++ src/CH01/SystemOutPrintln.java
@@ -0,0 +1,13 @@
+package CH01;
+
+public class SystemOutPrintln { // 콘솔 화면 출력 예제
+    public static void main(String[] args)
+    {
+        System.out.println(7);
+        System.out.println(3.15); // 6~7라인) 숫자만 출력할 때엔 큰따옴표 필요하지 않음
+        System.out.println("3 + 5 = "+ 8);
+        System.out.println(3.15 + "는 실수입니다.");
+        System.out.println("3 + 5" + " 의 연산 결과는 8입니다."); // 8~10라인) 함께 이어서 출력할 대상을 + 기호로 묶음
+        System.out.println(3 + 5); // 11라인) 수를 대상으로 하는 + 기호는 덧셈을 수행함
+    }
+}
