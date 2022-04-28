4564faab0eda42d22c129dabe25023cc62b9cf54
류지헌 <rjhwind94@gmail.com>
2022-04-28 09:40:59 +0900

    FirstJavaProgram.java 학습 주석 작성

--- src/CH01/FirstJavaProgram.java
+++ src/CH01/FirstJavaProgram.java
@@ -1,9 +1,10 @@
-package CH01;
+package CH01; // 패키지명 CH01;
 
-public class FirstJavaProgram {
+public class FirstJavaProgram { // 클래스명 FirstJavaProgram
     // 첫 Java 프로그램 작성
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java");
+    public static void main(String[] args)// 메서드명 main
+    {
+        System.out.println("Welcome to Java"); // 문장의 끝에는 반드시 세미콜론(;)을 붙여 표시한다.
         System.out.println("First Java Program");
-    }
-}
+    } // Java는 중괄호를 이용하여 영역이나 경계를 형성한다. (여기서는 클래스의 영역 구분)
+} // 따라서 Java는 중괄호를 이용해서 클래스와 메소드의 영역을 구분한다.
